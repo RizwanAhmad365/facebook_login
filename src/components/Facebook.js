357b7f3ffde41a3e74841fbd107690e8345e96b0
Rizwan Ahmad <rizahmad365@gmail.com>
2021-11-13 14:43:06 +0545

    Completed the login button

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import FacebookLogin from "react-facebook-login";
 
 const Facebook = () => {
-  const state = {
-    isLoggedIn: false,
-    userId: "",
-    name: "",
-    email: "",
-    picture: "",
-  };
+  const [loginStatus, setLoginStatus] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [picture, setPicture] = useState("");
 
   const responseFacebook = (response) => {
-    console.log(response);
+    setLoginStatus(true);
+    setUserId(response.userId);
+    setName(response.name);
+    setEmail(response.email);
+    setPicture(response.picture.data.url);
   };
   const componentClicked = () => {
     console.log("Clicked");
   };
 
   let fbContent = null;
-  if (state.isLoggedIn) {
+  if (loginStatus) {
+    fbContent = (
+      <div
+        style={{
+          width: "400px",
+          margin: "auto",
+          background: "lightgray",
+          padding: "20px",
+        }}
+      >
+        <img src={picture} alt={name} />
+        <h2 style={{ color: "#333" }}>Welcome {name}</h2>
+        Email: {email}
+      </div>
+    );
   } else {
     fbContent = (
       <FacebookLogin
