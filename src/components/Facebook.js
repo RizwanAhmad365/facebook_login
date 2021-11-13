4b73621baf8ae7e18067353f636162e74de0b125
Rizwan Ahmad <rizahmad365@gmail.com>
2021-11-13 14:17:36 +0545

    Create the button and added it into App.js

---
+++ src/components/Facebook.js
@@ -0,0 +1,37 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
+
+const Facebook = () => {
+  const state = {
+    isLoggedIn: false,
+    userId: "",
+    name: "",
+    email: "",
+    picture: "",
+  };
+
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
+  const componentClicked = () => {
+    console.log("Clicked");
+  };
+
+  let fbContent = null;
+  if (state.isLoggedIn) {
+  } else {
+    fbContent = (
+      <FacebookLogin
+        appId="3137067039857675"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={componentClicked}
+        callback={responseFacebook}
+      />
+    );
+  }
+  return <div>{fbContent}</div>;
+};
+
+export default Facebook;
